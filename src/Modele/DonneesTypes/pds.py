e234ae3d9022640d43264ef3965a048f700f62fd
bougnat <96043856+camille4426@users.noreply.github.com>
2026-01-07 15:45:52 +0100

    Update pds.py

--- src/Modele/DonneesTypes/pds.py
+++ src/Modele/DonneesTypes/pds.py
@@ -1,17 +1,16 @@
-from ..modele import Modele
 import nibabel as nib
 import numpy as np
+from src.logger import get_logger
 
-class PDS(Modele):
+class PDS:
     # Initialisation de la classe
-    def __init__(self, nom: str, fichier_path: str):
-        super().__init__(nom)
-        self.fichier_path = fichier_path
+    def __init__(self, nom: str, fichier: UploadFile):
+        self.fichier = fichier
 
     #def affichage_front(self):
         # 
 
     # Retourne les informations de la classe
     def summary(self):
-        shape = self.data.shape if self.data is not None else None
-        return {"type": "IRM", "nom": self.nom, "shape": shape}
+        #shape = self.data.shape if self.data is not None else None
+        return {"type": "PDS", "nom": self.nom, "shape": shape}
