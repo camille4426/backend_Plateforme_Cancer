e49d9729dfec3d729a40b37ebf1e798a00d6ebf9
bougnat <96043856+camille4426@users.noreply.github.com>
2026-01-07 15:44:57 +0100

    Update mrsi.py

--- src/Modele/DonneesTypes/mrsi.py
+++ src/Modele/DonneesTypes/mrsi.py
@@ -2,16 +2,15 @@ from ..modele import Modele
 import nibabel as nib
 import numpy as np
 
-class MRSI(Modele):
+class MRSI:
     # Initialisation de la classe
-    def __init__(self, nom: str, fichier_path: str):
-        super().__init__(nom)
-        self.fichier_path = fichier_path
+    def __init__(self, nom: str, fichier: UploadFile):
+        self.fichier = fichier #conservation du fichier
 
     #def affichage_front(self):
         # 
 
     # Retourne les informations de la classe
     def summary(self):
-        shape = self.data.shape if self.data is not None else None
-        return {"type": "IRM", "nom": self.nom, "shape": shape}
+        #shape = self.data.shape if self.data is not None else None
+        return {"type": "MRSI", "nom": self.nom, "shape": shape}
